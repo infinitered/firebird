840637d63cd19745a837e5b7b7e50c103d00e070
Ryan Linton <ryan@infinite.red>
2017-08-10 17:31:42 -0700

    Fix web.ex

--- template/$PROJECT_NAME$/lib/$PROJECT_NAME$_web.ex
+++ template/$PROJECT_NAME$/lib/$PROJECT_NAME$_web.ex
@@ -19,7 +19,7 @@ defmodule <%= @project_name_camel_case %>Web do
 
   def controller do
     quote do
-      use Phoenix.Controller, namespace: XxxxxWeb
+      use Phoenix.Controller, namespace: <%= @project_name_camel_case %>Web
       import Plug.Conn
       import <%= @project_name_camel_case %>Web.Router.Helpers
       import <%= @project_name_camel_case %>Web.Gettext
@@ -32,7 +32,7 @@ defmodule <%= @project_name_camel_case %>Web do
                         namespace: <%= @project_name_camel_case %>Web
 
       # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
+      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
